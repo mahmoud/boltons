d64f944b3ee23f191daee685318aad0f4337fb4d
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-02-20 02:09:02 -0800

    scratch that, regexes still faster

--- boltons/strutils.py
+++ boltons/strutils.py
@@ -2,16 +2,26 @@
 import re
 import string
 
-_camel2under_re = re.compile('((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
 _punct_ws_str = string.punctuation + string.whitespace
+_punct_re = re.compile('[' + _punct_ws_str + ']+')
+_camel2under_re = re.compile('((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
 
 
 def slugify(text, delim='_'):
-    return delim.join(split_punct_ws(text).lower())
+    """
+    A basic function that turns text full of scary characters
+    (i.e., punctuation and whitespace), into a relatively safe
+    lowercased string separated only by the delimiter specified
+    by 'delim', which defaults to '_'.
+
+    >>> 'First post! Hi!!!!~1    '
+    'first_post_hi_1'
+    """
+    return delim.join(split_punct_ws(text)).lower()
 
 
-def split_punct_ws(text, _punct=_punct_ws_str):
-    return [w for w in text.split(_punct) if w]
+def split_punct_ws(text):
+    return [w for w in _punct_re.split(text) if w]
 
 
 def camel2under(string):
