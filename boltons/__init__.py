f9d4e92addd7e25f713a9527a737b27dc90ef222
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-02-28 23:47:23 -0800

    a bit of packaging

---
+++ boltons/__init__.py
@@ -0,0 +1,9 @@
+
+# I'll do a better job of this soon
+
+from compat import *
+
+from strutils import *
+from excutils import *
+from iterutils import *
+from funcutils import *
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+    boltons
+    ~~~~~~~
+
+    Functionality that should be in the standard library. Like
+    builtins. But Boltons.
+
+    Otherwise known as "everyone's util.py," but cleaned up and
+    tested.
+
+    :copyright: (c) 2013 by Mahmoud Hashemi
+    :license: BSD, see LICENSE for more details.
+
+"""
+
+import sys
+from setuptools import setup
+
+
+__author__ = 'Mahmoud Hashemi'
+__version__ = '0.1'
+__contact__ = 'mahmoudrhashemi@gmail.com'
+__url__ = 'https://github.com/mahmoud/boltons'
+__license__ = 'BSD'
+
+
+if sys.version_info >= (3,):
+    raise NotImplementedError("boltons Python 3 support en route to your location")
+
+
+setup(name='boltons',
+      version=__version__,
+      description="When they're not builtins, they're boltons.",
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['boltons'],
+      include_package_data=True,
+      zip_safe=False,
+      license=__license__,
+      platforms='any',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7', ]
+      )
