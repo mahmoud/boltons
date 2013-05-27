6bcd548c1aae5925eea252b40e6a18a5f9b24dc5
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-26 23:01:51 -0700

    add the super simple BasicCache, a size-limited defaultdict-like thing

--- boltons/dictutils.py
+++ boltons/dictutils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
+from collections import OrderedDict, deque
 
 try:
     from compat import make_sentinel
@@ -108,3 +108,36 @@ class OrderedMultiDict(OrderedDict):
 
 
 MultiDict = OrderedMultiDict
+
+
+class BasicCache(dict):
+    """\
+    a.k.a, SizeLimitedDefaultDict.
+    """
+    def __init__(self, size, on_miss):
+        super(BasicCache, self).__init__()
+        self.size = size
+        self.on_miss = on_miss
+        self._queue = deque()
+
+    def __missing__(self, key):
+        ret = self.on_miss(key)
+        self[key] = ret
+        self._queue.append(key)
+        if len(self._queue) > self.size:
+            old = self._queue.popleft()
+            del self[old]
+        return ret
+
+    try:
+        from collections import defaultdict
+    except ImportError:
+        # no defaultdict means that __missing__ isn't supported in
+        # this version of python, so we define __getitem__
+        def __getitem__(self, key):
+            try:
+                return super(BasicCache, self).__getitem__(key)
+            except KeyError:
+                return self.__missing__(key)
+    else:
+        del defaultdict
