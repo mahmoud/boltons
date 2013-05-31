872f72632ed64fd622e5ceac21d1e744abdaf993
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-30 22:41:25 -0700

    add a relative datetime function in a new module: timeutils

---
+++ boltons/timeutils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+def relative_datetime(d, other=None):
+    # TODO: add decimal rounding factor (default 0)
+    if other is None:
+        other = datetime.datetime.utcnow()
+    diff = other - d
+    s, days = diff.seconds, diff.days
+    if days > 7 or days < 0:
+        return d.strftime('%d %b %y')
+    elif days == 1:
+        return '1 day ago'
+    elif days > 1:
+        return '{0} days ago'.format(diff.days)
+    elif s < 5:
+        return 'just now'
+    elif s < 60:
+        return '{0} seconds ago'.format(s)
+    elif s < 120:
+        return '1 minute ago'
+    elif s < 3600:
+        return '{0} minutes ago'.format(s / 60)
+    elif s < 7200:
+        return '1 hour ago'
+    else:
+        return '{0} hours ago'.format(s / 3600)
