77fbe64ced0148f58067e85735bb5c30d85b95de
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-08-25 23:04:51 -0700

    switch OMD benching to use lithoxyl for stats, etc., and compare against other mapping implementations

--- boltons/_bench_omd.py
+++ boltons/_bench_omd.py
@@ -17,27 +17,56 @@ log = lithoxyl.logger.BaseLogger('bench_stats', sinks=[q_sink])
 try:
     profile
 except NameError:
-    times = 10
-    size = 5000
-    redun = 1
+    times = 5
+    size = 6000
+    redun = 2
 else:
     times = 1
     size = 10000
     redun = 10
 
 _rng = range(size / redun) * redun
+_unique_keys = set(_rng)
+_bad_rng = range(size, size + size)
 _pairs = zip(_rng, _rng)
 
 
-for impl in (OMD, WOMD, MultiDict, dict):
-    q_sink = lithoxyl.sinks.QuantileSink()
-    log = lithoxyl.logger.BaseLogger('bench_stats', sinks=[q_sink])
-    print
-    print '+ %s.%s' % (impl.__module__, impl.__name__)
-    for i in range(times):
-        with log.info('bench_omd') as r:
-            impl(_pairs)
+def bench():
+    for impl in (OMD, WOMD, MultiDict, OD, dict):
+        q_sink = lithoxyl.sinks.QuantileSink()
+        log = lithoxyl.logger.BaseLogger('bench_stats', sinks=[q_sink])
+        print
+        print '+ %s.%s' % (impl.__module__, impl.__name__)
+        for i in range(times):
+            with log.info('bench_omd') as r:
+                # initialization
+                d = impl(_pairs)
+                # just iteration
+                for j in range(times):
+                    [_ for _ in d.iteritems()]
 
-    best_msecs = q_sink.min * 1000
-    median_msecs = q_sink.median * 1000
-    print ' > ran %d loops of %d items each, best time: %g ms, median time: %g ms' % (times, size, best_msecs, median_msecs)
+                # access existent keys
+                for j in range(times):
+                    for k in _rng:
+                        d[k]
+                # access non-existent keys
+                for k in _bad_rng:
+                    try:
+                        d[k]
+                    except KeyError:
+                        pass
+                # pop
+                for k in _unique_keys:
+                    d.pop(k)
+                assert not d
+
+        best_msecs = q_sink.min * 1000
+        median_msecs = q_sink.median * 1000
+        print ' > ran %d loops of %d items each, best time: %g ms, median time: %g ms' % (times, size, best_msecs, median_msecs)
+
+    print
+    return
+
+
+if __name__ == '__main__':
+    bench()
