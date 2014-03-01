52daf1ea5166d069a6479373148fa4710a239f8f
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-02-28 22:55:13 -0800

    adding pure-Python implementation of Gob's algorithm

---
+++ boltons/easterutils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def gobs_program():
+    """
+    A pure-Python implementation of Gob's Algorithm (2013). A brief
+    explanation can be found here:
+    https://www.youtube.com/watch?v=JbnjusltDHk
+    """
+    while True:
+        print "PENUS",
+
+
+if __name__ == '__main__':
+    gobs_program()
