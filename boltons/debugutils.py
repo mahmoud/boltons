8ffbc455ce6d87cbd842d902e5f911cf41c012c1
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-03-04 22:33:17 -0800

    add pdb excepthook and some helpful docstrings

--- boltons/debugutils.py
+++ boltons/debugutils.py
@@ -2,6 +2,15 @@
 
 
 def pdb_on_signal(signalnum=None):
+    """
+    Installs a signal handler for `signalnum` (default SIGINT/keyboard
+    interrupt/ctrl-c) that launches a pdb breakpoint. This can be
+    useful for debugging infinite loops and getting into deep call
+    stacks.
+
+    This may have unintended results when used in highly
+    threaded/concurrent code.
+    """
     import pdb
     import signal
     if not signalnum:
@@ -16,3 +25,28 @@ def pdb_on_signal(signalnum=None):
 
     signal.signal(signalnum, pdb_int_handler)
     return
+
+
+def pdb_on_exception(limit=100):
+    """
+    Installs a handler which, instead of exiting, attaches a
+    post-mortem pdb console whenever an unhandled exception is
+    encountered.
+
+    To restore default behavior, just do:
+
+    `sys.excepthook = sys.__excepthook__`
+
+    A similar effect can be achieved with the following command:
+
+    `python -m pdb your_code.py`
+    """
+    import pdb
+    import sys
+    import traceback
+
+    def pdb_excepthook(exc_type, exc_val, exc_tb):
+        traceback.print_tb(exc_tb, limit=limit)
+        pdb.post_mortem(exc_tb)
+
+    sys.excepthook = pdb_excepthook
