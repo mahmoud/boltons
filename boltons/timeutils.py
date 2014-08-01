8021d0336b81a8896f9a3983ff3492f275c83277
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-08-01 01:08:41 -0700

    fix .keys -> .keys()

--- boltons/timeutils.py
+++ boltons/timeutils.py
@@ -60,7 +60,7 @@ def parse_td(text):
             unit_key = _PARSE_TD_KW_MAP[unit]
         except KeyError:
             raise ValueError('invalid time unit %r, expected one of %r'
-                             % (unit, _PARSE_TD_KW_MAP.keys))
+                             % (unit, _PARSE_TD_KW_MAP.keys()))
         try:
             value = float(value)
         except ValueError:
