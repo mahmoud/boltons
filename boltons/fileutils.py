3895c7800f70dee7f5ea6cfa2fccaff39f0dad8b
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-02-07 21:58:58 -0800

    refactor atomic_save to put more into the context manager. works like a charm.

--- boltons/fileutils.py
+++ boltons/fileutils.py
@@ -2,8 +2,11 @@
 
 import os
 import stat
+import errno
+import tempfile
 
-VALID_CHARS = 'rwx'
+VALID_PERM_CHARS = 'rwx'
+
 
 class FilePerms(object):
     # TODO: consider more than the lower 9 bits
@@ -20,14 +23,14 @@ class FilePerms(object):
             if cur == value:
                 return
             try:
-                invalid_chars = str(value).translate(None, VALID_CHARS)
+                invalid_chars = str(value).translate(None, VALID_PERM_CHARS)
             except (TypeError, UnicodeEncodeError):
                 raise TypeError('expected string, not %r' % value)
             if invalid_chars:
                 raise ValueError('got invalid chars %r in permission'
                                  ' specification %r, expected empty string'
                                  ' or one or more of %r'
-                                 % (invalid_chars, value, VALID_CHARS))
+                                 % (invalid_chars, value, VALID_PERM_CHARS))
             unique_chars = ''.join(set(value))
             setattr(fp_obj, self.attribute, unique_chars)
             self._update_integer(fp_obj, unique_chars)
@@ -76,84 +79,88 @@ class FilePerms(object):
                 % (cn, self.user, self.group, self.other))
 
 
-"""
-def validate(file_path):
-    pass
-
-with success_swap(file_to_write, validate=validate) as f:
-    f.write(urllib.get(url))
-
-"""
-
-import errno
-import tempfile
+def atomic_save(dest_path, **kwargs):
+    return AtomicSaver(dest_path, **kwargs)
 
 
-def success_swap(dest_path, validate=None, **kwargs):
-    overwrite = kwargs.pop('overwrite', True)
-    part_file = kwargs.pop('part_file', None)
-    text_mode = kwargs.pop('text_mode', False)
-    if kwargs:
-        raise TypeError('unexpected kwargs: %r' % kwargs.keys)
-    dest_path = os.path.abspath(dest_path)
-    if os.path.isfile(dest_path):
-        if not overwrite:
-            raise OSError(errno.EEXIST,
-                          'Overwrite disabled and file already exists',
-                          dest_path)
-    dest_dir = os.path.dirname(dest_path)
-    if not part_file:
-        part_path = dest_path + '.part'
+def _atomic_rename(path, new_path, overwrite=False):
+    if overwrite:
+        os.rename(path, new_path)
     else:
-        part_path = os.path.join(dest_dir, part_path)
-
-    # this tests for a writable directory with rename permissions
-    # early, as we may be writing to the part file for a while (not
-    # using os.access because of the potential issues of effective vs
-    # real privileges)
-    part_fd, tmp_part_path = tempfile.mkstemp(dir=dest_dir, text=text_mode)
-    os.rename(tmp_part_path, part_path)
-    mode = 'w+' if text_mode else 'w+b'
-    part_file = open(part_path, mode)  # os.fdopen(part_fd, mode)
-    #part_file.name = part_path
-    ss = SuccessSwapper(part_file, dest_path, validate, overwrite)
-    # import pdb;pdb.set_trace()
-    return ss
+        os.link(path, new_path)
+        os.unlink(path)
 
 
-class SuccessSwapper(object):
-    def __init__(self, part_file, dest_path, validate, overwrite):
-        self.part_file = part_file
-        self.part_path = part_file.name
+class AtomicSaver(object):
+    def __init__(self, dest_path, **kwargs):
         self.dest_path = dest_path
-        self.validate = validate
-        self.overwrite = overwrite
-        self.rm_part_on_exc = True
+        self.overwrite = kwargs.pop('overwrite', True)
+        self.overwrite_part = kwargs.pop('overwrite_partfile', True)
+        self.part_filename = kwargs.pop('part_file', None)
+        self.text_mode = kwargs.pop('text_mode', False)  # for windows
+        self.rm_part_on_exc = kwargs.pop('rm_part_on_exc', True)
+        self._open = kwargs.pop('open_func', open)
+        self._open_kwargs = kwargs.pop('open_kwargs', {})
+        if kwargs:
+            raise TypeError('unexpected kwargs: %r' % kwargs.keys)
+
+        self.dest_path = os.path.abspath(self.dest_path)
+        self.dest_dir = os.path.dirname(self.dest_path)
+        if not self.part_filename:
+            self.part_path = dest_path + '.part'
+        else:
+            self.part_path = os.path.join(self.dest_dir, self.part_path)
+        self.mode = 'w+' if self.text_mode else 'w+b'
+
+        self.part_file = None
+
+    def setup(self):
+        if os.path.lexists(self.dest_path):
+            if not self.overwrite:
+                raise OSError(errno.EEXIST,
+                              'Overwrite disabled and file already exists',
+                              self.dest_path)
+        # this tests for a writable directory with rename permissions
+        # early, as we may be writing to the part file for a while (not
+        # using os.access because of the potential issues of effective vs
+        # real privileges)
+        _, tmp_part_path = tempfile.mkstemp(dir=self.dest_dir,
+                                            text=self.text_mode)
+        try:
+            _atomic_rename(tmp_part_path, self.part_path,
+                           overwrite=self.overwrite_part)
+        except OSError:
+            os.unlink(tmp_part_path)
+            raise
+
+        self.part_file = self._open(self.part_path, self.mode,
+                                    **self._open_kwargs)
 
     def __enter__(self):
+        self.setup()
         return self.part_file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
             if self.rm_part_on_exc:
+                try:
+                    os.unlink(self.part_path)
+                except:
+                    pass
+            return
+        try:
+            _atomic_rename(self.part_path, self.dest_path,
+                           overwrite=self.overwrite)
+        except OSError:
+            if self.rm_part_on_exc:
                 os.unlink(self.part_path)
-            return
-        if self.validate:
-            if not self.validate(self.part_path):
-                # TODO raise exception or just return?
-                return
-        if not self.overwrite and os.path.isfile(self.dest_path):
-            # TODO: again, raise or just return
-            return
-
-        os.rename(self.part_path, self.dest_path)
-
-    def __getattr__(self, attr):
-        return getattr(self.part_file, attr)
+        return
 
 
-#with success_swap('/home/mahmoud/tmp/final.txt') as f:
-#    f.write('lol')
+#with atomic_save('/home/mahmoud/tmp/final.txt') as f:
+#    f.write('rofl')
+#    raise ValueError('nope')
+#    f.write('\n')
 
 
 if __name__ == '__main__':
