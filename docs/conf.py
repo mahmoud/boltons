bee2ecce5a41c49a3fc9da4c89567c3f117d6f85
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-03-25 01:07:29 -0700

    progress on intersphinxin

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/2.7', None)}
 
 
 # -- Options for HTML output ----------------------------------------------
