07bbfed644670b7edd7450aadb768b543836bd27
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-03-30 20:56:34 -0700

    Enable napoleon for docstrings.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
