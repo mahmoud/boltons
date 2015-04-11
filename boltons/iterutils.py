9d1e403ed939d20101bf4db715c48cd20524f680
Martín Gaitán <gaitan@gmail.com>
2015-04-11 14:51:12 -0300

    added function one

--- boltons/iterutils.py
+++ boltons/iterutils.py
@@ -346,3 +346,38 @@ def unique_iter(src, key=None):
             seen.add(k)
             yield i
     return
+
+
+def one(src, cmp=None):
+    """
+    Return the object in the given iterable ``src`` that evaluates to True.
+
+    If the given iterable has more than one object that evaluates to True,
+    or if there is no object that fulfills such condition, return False.
+
+    If a callable ``cmp`` is given, it's used to evaluate each element.
+
+    >>> one((True, False, False))
+    True
+    >>> one((True, False, True))
+    False
+    >>> one((0, 0, 'a'))
+    'a'
+    >>> one((0, False, None))
+    False
+    >>> one((True, True))
+    False
+    >>> bool(one(('', 1)))
+    True
+    >>> one((10, 20, 30, 42), lambda i: i > 40)
+    42
+
+    See https://github.com/mgaitan/one for further use cases
+    """
+    the_one = False
+    for i in src:
+        if cmp(i) if cmp else i:
+            if the_one:
+                return False
+            the_one = i
+    return the_one
