c4a489d853b9130744ce2794be4a1cb79331b4b3
Anthony Sottile <asottile@umich.edu>
2015-04-20 13:52:04 -0700

    Fix multiline exception messages

--- boltons/tbutils.py
+++ boltons/tbutils.py
@@ -753,7 +753,7 @@ class ParsedException(object):
             frames.append(frame_dict)
 
         try:
-            exc_line = tb_lines[line_no]
+            exc_line = '\n'.join(tb_lines[line_no:])
             exc_type, _, exc_msg = exc_line.partition(':')
         except Exception:
             exc_type, exc_msg = '', ''
