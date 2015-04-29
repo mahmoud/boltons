fb5b2621f371bf4fd6e6ff59630fb699f2bb0659
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-04-29 02:04:42 -0700

    refactoring ceil and floor to be simpler, more like stdlib. also modifying the respective tests. discussion on issue #36

--- boltons/mathutils.py
+++ boltons/mathutils.py
@@ -1,77 +1,57 @@
 """This module provides useful math functions on top of Python's
 built-in :mod:`math` module.
 """
+from math import ceil as _ceil, floor as _floor
 import bisect
 
 
-def ceil_from_iter(src, x, allow_equal=True, sorted_src=False):
-    """Return the ceiling of *x*, the smallest integer or float from
-    *src* that is greater than [or equal to] *x*.
+def ceil(x, options=None):
+    """Return the ceiling of *x*. If *options* is set, return the smallest
+    integer or float from *options* that is greater than or equal to
+    *x*.
 
     Args:
-        src (iterable):     Iterable of arbitrary numbers (ints or floats).
-        x (int or float):   Number to be tested.
-        allow_equal (bool): Defaults to ``True``. Allows equality to
-            be ignored if set to ``False``.
-        sorted_src (bool): Defaults to ``False``. Allows potential
-           performance increase for large *src* iterable if set to
-           ``False``, as long as *src* is pre-sorted.
-
-    >>> VALID_CABLE_CSA = [1.5, 2.5, 4, 6, 10, 25, 35, 50]
-    >>> ceil_from_iter(VALID_CABLE_CSA, 3.5)
-    4
-    >>> ceil_from_iter(VALID_CABLE_CSA, 4)
-    4
-    >>> ceil_from_iter(VALID_CABLE_CSA, 4, allow_equal=False)
-    6
-    """
-    if not sorted_src:
-        src = sorted(src)
-
-    if allow_equal:
-        i = bisect.bisect_left(src, x)
-        if i != len(src):
-            return src[i]
-        raise ValueError("No ceiling value in source iterable greater than or equal to:", x)
-    else:
-        i = bisect.bisect_right(src, x)
-        if i != len(src):
-            return src[i]
-        raise ValueError("No ceiling value in source iterable greater than:", x)
-
-
-def floor_from_iter(src, x, allow_equal=True, sorted_src=False):
-    """Return the floor of *x*, the largest integer or float from *src*
-    that is less than [or equal to] *x*.
-
-    Args:
-        src (iterable): Iterable of arbitrary numbers (ints or floats).
         x (int or float): Number to be tested.
-        allow_equal (bool): Defaults to ``True``. Allows equality to
-            be ignored if set to ``False``.
-        sorted_src (bool): Defaults to ``False``. Allows potential
-            performance increase for large *src* iterable if set to
-            ``False``, as long as *src* is pre-sorted.
+        options (iterable): Optional terable of arbitrary numbers
+          (ints or floats).
 
     >>> VALID_CABLE_CSA = [1.5, 2.5, 4, 6, 10, 25, 35, 50]
-    >>> floor_from_iter(VALID_CABLE_CSA, 3.5)
-    2.5
-    >>> floor_from_iter(VALID_CABLE_CSA, 2.5)
-    2.5
-    >>> floor_from_iter(VALID_CABLE_CSA, 2.5, allow_equal=False)
-    1.5
+    >>> ceil(3.5, options=VALID_CABLE_CSA)
+    4
+    >>> ceil(4, options=VALID_CABLE_CSA)
+    4
     """
+    if options is None:
+        return _ceil(x)
+    options = sorted(options)
+    i = bisect.bisect_left(options, x)
+    if i == len(options):
+        raise ValueError("no ceil options greater than or equal to: %r" % x)
+    return options[i]
 
-    if not sorted_src:
-        src = sorted(src)
 
-    if allow_equal:
-        i = bisect.bisect_right(src, x)
-        if i:
-            return src[i-1]
-        raise ValueError("No floor value in source iterable less than or equal to:", x)
-    else:
-        i = bisect.bisect_left(src, x)
-        if i:
-            return src[i-1]
-        raise ValueError("No floor value in source iterable less than:", x)
+def floor(x, options=None):
+    """Return the floor of *x*. If *options* is set, return the largest
+    integer or float from *options* that is less than or equal to
+    *x*.
+
+    Args:
+        x (int or float): Number to be tested.
+        options (iterable): Optional iterable of arbitrary numbers
+          (ints or floats).
+
+    >>> VALID_CABLE_CSA = [1.5, 2.5, 4, 6, 10, 25, 35, 50]
+    >>> floor(3.5, options=VALID_CABLE_CSA)
+    2.5
+    >>> floor(2.5, options=VALID_CABLE_CSA)
+    2.5
+
+    """
+    if options is None:
+        return _floor(x)
+    options = sorted(options)
+
+    i = bisect.bisect_right(options, x)
+    if not i:
+        raise ValueError("no floor options less than or equal to: %r" % x)
+    return options[i - 1]
--- tests/mathutils_test.py
+++ tests/mathutils_test.py
@@ -1,96 +1,56 @@
-import unittest
-import boltons.mathutils as bmu
+
+from pytest import raises
+from boltons.mathutils import ceil, floor
 
 
-class TestCeilAndFloor(unittest.TestCase):
+OPTIONS = [1618, 1378, 166, 1521, 2347, 2016, 879, 2123,
+           269.3, 1230, 66, 425.2, 250, 2399, 2314, 439,
+           247, 208, 379, 1861]
+OPTIONS_SORTED = sorted(OPTIONS)
+OUT_OF_RANGE_LOWER = 60
+OUT_OF_RANGE_UPPER = 2500
+VALID_LOWER = 247
+VALID_UPPER = 2314
+VALID_BETWEEN = 248.5
 
-    def setUp(self):
-        self.BIG_LIST = [1618, 1378, 166, 1521, 2347, 2016, 879, 2123, 269.3, 1230, 66, 425.2, 250, 2399, 2314, 439,
-                         247, 208, 379, 1861]
-        self.BIG_LIST_SORTED = sorted(self.BIG_LIST)
-        self.OUT_OF_RANGE_LOWER = 60
-        self.OUT_OF_RANGE_UPPER = 2500
-        self.VALID_LOWER = 247
-        self.VALID_UPPER = 2314
-        self.VALID_BETWEEN = 248.5
 
-    # Tests for boltons.mathutils.ceil_from_iter()
-    def test_ceil_from_iter_default(self):
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_LOWER), self.VALID_LOWER)
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_UPPER), self.VALID_UPPER)
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_BETWEEN), 250)
+def test_ceil_basic():
+    assert ceil(VALID_LOWER, OPTIONS) == VALID_LOWER
+    assert ceil(VALID_UPPER, OPTIONS) == VALID_UPPER
+    assert ceil(VALID_BETWEEN, OPTIONS) == 250
 
-    def test_ceil_from_iter_default_allow_equal_false(self):
-        self.assertNotEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_LOWER, allow_equal=False), self.VALID_LOWER)
-        self.assertNotEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_UPPER, allow_equal=False), self.VALID_UPPER)
 
-    def test_ceil_from_iter_unsorted(self):
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_LOWER),
-                         bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER))
+def test_ceil_sorted():
+    assert ceil(VALID_LOWER, OPTIONS) == ceil(VALID_LOWER, OPTIONS_SORTED)
+    assert ceil(VALID_UPPER, OPTIONS) == ceil(VALID_UPPER, OPTIONS_SORTED)
+    assert ceil(VALID_BETWEEN, OPTIONS) == ceil(VALID_BETWEEN, OPTIONS_SORTED)
 
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_UPPER),
-                         bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER))
 
-    def test_ceil_from_iter_sorted_src_true(self):
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_LOWER),
-                         bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER, sorted_src=True))
+def test_ceil_oor_lower():
+    assert min(OPTIONS) == ceil(OUT_OF_RANGE_LOWER, OPTIONS)
 
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST, self.VALID_UPPER),
-                         bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER, sorted_src=True))
 
-    def test_ceil_from_iter_sorted(self):
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER), self.VALID_LOWER)
-        self.assertEqual(bmu.ceil_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER), self.VALID_UPPER)
+def test_ceil_oor_upper():
+    with raises(ValueError):
+        ceil(OUT_OF_RANGE_UPPER, OPTIONS)
 
-    def test_ceil_from_iter_out_of_range_lower(self):
-        expected = min(self.BIG_LIST)
-        actual = bmu.ceil_from_iter(self.BIG_LIST, self.OUT_OF_RANGE_LOWER)
-        self.assertEqual(expected, actual)
 
-    def test_ceil_from_iter_exception_out_of_range_upper(self):
-        self.assertRaises(ValueError, bmu.ceil_from_iter, self.BIG_LIST, self.OUT_OF_RANGE_UPPER)
+def test_floor_basic():
+    assert floor(VALID_LOWER, OPTIONS) == VALID_LOWER
+    assert floor(VALID_UPPER, OPTIONS) == VALID_UPPER
+    assert floor(VALID_LOWER, OPTIONS) == 247
 
-    def test_ceil_from_iter_exception_out_of_range_upper_allow_equal_false(self):
-        self.assertRaises(ValueError, bmu.ceil_from_iter, self.BIG_LIST, self.OUT_OF_RANGE_UPPER, allow_equal=False)
 
-    # Tests for boltons.mathutils.floor_from_iter()
-    def test_floor_from_iter_default(self):
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_LOWER), self.VALID_LOWER)
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_UPPER), self.VALID_UPPER)
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_BETWEEN), 247)
+def test_floor_sorted():
+    assert floor(VALID_LOWER, OPTIONS) == floor(VALID_LOWER, OPTIONS_SORTED)
+    assert floor(VALID_UPPER, OPTIONS) == floor(VALID_UPPER, OPTIONS_SORTED)
+    assert floor(VALID_BETWEEN, OPTIONS) == floor(VALID_BETWEEN, OPTIONS_SORTED)
 
-    def test_floor_from_iter_default_allow_equal_false(self):
-        self.assertNotEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_LOWER, allow_equal=False), self.VALID_LOWER)
-        self.assertNotEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_UPPER, allow_equal=False), self.VALID_UPPER)
 
-    def test_floor_from_iter_unsorted(self):
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_LOWER),
-                         bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER))
+def test_floor_oor_upper():
+    assert max(OPTIONS) == floor(OUT_OF_RANGE_UPPER, OPTIONS)
 
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_UPPER),
-                         bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER))
 
-    def test_floor_from_iter_sorted_src_true(self):
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_LOWER),
-                         bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER, sorted_src=True))
-
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST, self.VALID_UPPER),
-                         bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER, sorted_src=True))
-
-    def test_floor_from_iter_sorted(self):
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_LOWER), self.VALID_LOWER)
-        self.assertEqual(bmu.floor_from_iter(self.BIG_LIST_SORTED, self.VALID_UPPER), self.VALID_UPPER)
-
-    def test_floor_from_iter_out_of_range_upper(self):
-        expected = max(self.BIG_LIST)
-        actual = bmu.floor_from_iter(self.BIG_LIST, self.OUT_OF_RANGE_UPPER)
-        self.assertEqual(expected, actual)
-
-    def test_floor_from_iter_exception_out_of_range_lower(self):
-        self.assertRaises(ValueError, bmu.floor_from_iter, self.BIG_LIST, self.OUT_OF_RANGE_LOWER)
-
-    def test_floor_from_iter_exception_out_of_range_lower_allow_equal_false(self):
-        self.assertRaises(ValueError, bmu.floor_from_iter, self.BIG_LIST, self.OUT_OF_RANGE_LOWER, allow_equal=False)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_floor_oor_lower():
+    with raises(ValueError):
+        floor(OUT_OF_RANGE_LOWER, OPTIONS)
