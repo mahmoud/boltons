e95c08d5727101f0574df98ddf35aed014798fb5
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-04-29 00:55:15 -0700

    various docstring formatting tweaks for to mathutils

--- boltons/mathutils.py
+++ boltons/mathutils.py
@@ -1,18 +1,21 @@
-"""This module provides useful math functions on top of Python's built-in :mod:`math` module.
+"""This module provides useful math functions on top of Python's
+built-in :mod:`math` module.
 """
 import bisect
 
 
 def ceil_from_iter(src, x, allow_equal=True, sorted_src=False):
-    """
-    Return the ceiling of *x*, the smallest integer or float from *src* that is greater than [or equal to] *x*.
+    """Return the ceiling of *x*, the smallest integer or float from
+    *src* that is greater than [or equal to] *x*.
 
     Args:
         src (iterable):     Iterable of arbitrary numbers (ints or floats).
         x (int or float):   Number to be tested.
-        allow_equal (bool): Defaults to ``True``. Allows equality to be ignored if set to ``False``.
-        sorted_src (bool):  Defaults to ``False``. Allows potential performance increase for large *src* iterable if set
-                            to ``False``, as long as *src* is pre-sorted.
+        allow_equal (bool): Defaults to ``True``. Allows equality to
+            be ignored if set to ``False``.
+        sorted_src (bool): Defaults to ``False``. Allows potential
+           performance increase for large *src* iterable if set to
+           ``False``, as long as *src* is pre-sorted.
 
     >>> VALID_CABLE_CSA = [1.5, 2.5, 4, 6, 10, 25, 35, 50]
     >>> ceil_from_iter(VALID_CABLE_CSA, 3.5)
@@ -22,7 +25,6 @@ def ceil_from_iter(src, x, allow_equal=True, sorted_src=False):
     >>> ceil_from_iter(VALID_CABLE_CSA, 4, allow_equal=False)
     6
     """
-
     if not sorted_src:
         src = sorted(src)
 
@@ -39,15 +41,17 @@ def ceil_from_iter(src, x, allow_equal=True, sorted_src=False):
 
 
 def floor_from_iter(src, x, allow_equal=True, sorted_src=False):
-    """
-    Return the floor of *x*, the largest integer or float from *src* that is less than [or equal to] *x*.
+    """Return the floor of *x*, the largest integer or float from *src*
+    that is less than [or equal to] *x*.
 
     Args:
-        src (iterable):     Iterable of arbitrary numbers (ints or floats).
-        x (int or float):   Number to be tested.
-        allow_equal (bool): Defaults to ``True``. Allows equality to be ignored if set to ``False``.
-        sorted_src (bool):  Defaults to ``False``. Allows potential performance increase for large *src* iterable if set
-                            to ``False``, as long as *src* is pre-sorted.
+        src (iterable): Iterable of arbitrary numbers (ints or floats).
+        x (int or float): Number to be tested.
+        allow_equal (bool): Defaults to ``True``. Allows equality to
+            be ignored if set to ``False``.
+        sorted_src (bool): Defaults to ``False``. Allows potential
+            performance increase for large *src* iterable if set to
+            ``False``, as long as *src* is pre-sorted.
 
     >>> VALID_CABLE_CSA = [1.5, 2.5, 4, 6, 10, 25, 35, 50]
     >>> floor_from_iter(VALID_CABLE_CSA, 3.5)
