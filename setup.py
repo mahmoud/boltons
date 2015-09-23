1d281790cf5cdb7dcb6ca127a2dd02ce6ac4e934
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-09-23 01:31:31 -0700

    bumping version for 15.1.0 release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '15.0.3dev'
+__version__ = '15.1.0'
 __contact__ = 'mahmoudrhashemi@gmail.com'
 __url__ = 'https://github.com/mahmoud/boltons'
 __license__ = 'BSD'
