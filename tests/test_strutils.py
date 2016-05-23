c669103ccbaf76982c8042be2f5a013a92afe64e
Adam Gibson <adam.gibson@tc4k.net>
2016-05-23 13:39:22 +0800

    Added range2list() and list2range() functions to strutils.py - added applicable tests to test_strutils.py

--- tests/test_strutils.py
+++ tests/test_strutils.py
@@ -24,3 +24,22 @@ def test_is_uuid():
     assert strutils.is_uuid(str(uuid.uuid4())) == True
     assert strutils.is_uuid(str(uuid.uuid4()), version=1) == False
     assert strutils.is_uuid(set('garbage')) == False
+
+
+def test_range2list():
+    assert strutils.range2list("1,3,5-8,10-11,15") == [1, 3, 5, 6, 7, 8, 10, 11, 15]
+
+    assert strutils.range2list("1,3,5-8,10-11,15,") == [1, 3, 5, 6, 7, 8, 10, 11, 15]
+    assert strutils.range2list(",1,3,5-8,10-11,15") == [1, 3, 5, 6, 7, 8, 10, 11, 15]
+    assert strutils.range2list(" 1, 3 ,5-8,10-11,15 ") == [1, 3, 5, 6, 7, 8, 10, 11, 15]
+    assert strutils.range2list("3,1,5-8,10-11,15") == [1, 3, 5, 6, 7, 8, 10, 11, 15]
+
+    assert strutils.range2list("5-8") == [5, 6, 7, 8]
+    assert strutils.range2list("8-5") == [5, 6, 7, 8]
+
+def test_list2range():
+    assert strutils.list2range([1, 3, 5, 6, 7, 8, 10, 11, 15]) == '1,3,5-8,10-11,15'
+    assert strutils.list2range([5, 6, 7, 8]) == '5-8'
+
+    assert strutils.list2range([1, 3, 5, 6, 7, 8, 10, 11, 15], delim_space=True) == '1, 3, 5-8, 10-11, 15'
+    assert strutils.list2range([5, 6, 7, 8], delim_space=True) == '5-8'
