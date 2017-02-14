b3a18a403de002a82713b2dbecfe47435edb38c2
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-02-14 01:51:10 -0800

    add windows inet_pton shim

--- boltons/urlutils.py
+++ boltons/urlutils.py
@@ -432,6 +432,37 @@ class URL(object):
         return not self == other
 
 
+try:
+    from socket import inet_pton
+except ImportError:
+    # from https://gist.github.com/nnemkin/4966028
+    import ctypes
+
+    class _sockaddr(ctypes.Structure):
+        _fields_ = [("sa_family", ctypes.c_short),
+                    ("__pad1", ctypes.c_ushort),
+                    ("ipv4_addr", ctypes.c_byte * 4),
+                    ("ipv6_addr", ctypes.c_byte * 16),
+                    ("__pad2", ctypes.c_ulong)]
+
+    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
+    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
+
+    def inet_pton(address_family, ip_string):
+        addr = _sockaddr()
+        addr.sa_family = address_family
+        addr_size = ctypes.c_int(ctypes.sizeof(addr))
+
+        if WSAStringToAddressA(ip_string, address_family, None, ctypes.byref(addr), ctypes.byref(addr_size)) != 0:
+            raise socket.error(ctypes.FormatError())
+
+        if address_family == socket.AF_INET:
+            return ctypes.string_at(addr.ipv4_addr, 4)
+        if address_family == socket.AF_INET6:
+            return ctypes.string_at(addr.ipv6_addr, 16)
+        raise socket.error('unknown address family')
+
+
 def parse_host(host):
     """\
     returns:
