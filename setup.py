cfb2f57ea0e7447c4bd07e13c613253ef9d0a0d0
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-12-15 23:39:37 -0800

    bump setup.py version for 17.2.1dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '17.2.0'
+__version__ = '17.2.1dev'
 __contact__ = 'mahmoudrhashemi@gmail.com'
 __url__ = 'https://github.com/mahmoud/boltons'
 __license__ = 'BSD'
