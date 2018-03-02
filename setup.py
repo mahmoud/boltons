6b0721b6aeda6d3ec6f5d31be7c741bc7fcc4635
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-03-02 00:16:52 -0800

    bump release for 18.0.1 development

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '18.0.0'
+__version__ = '18.0.1dev'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/boltons'
 __license__ = 'BSD'
