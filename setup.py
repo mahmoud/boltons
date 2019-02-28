d075f0b04dc1a8c9d05d7b9d53ae2a3846b4ba05
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-02-27 22:51:56 -0800

    couple of small cleanups

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ builtins, but Boltons.
 Otherwise known as, "everyone's util.py," but cleaned up and
 tested.
 
-Contains over 160 BSD-licensed utility types and functions that can be
+Contains over 230 BSD-licensed utility types and functions that can be
 used as a package or independently. `Extensively documented on Read
 the Docs <http://boltons.readthedocs.org>`_.
 """
