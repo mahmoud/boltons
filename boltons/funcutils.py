8dacb537db864e30ed6bb0e1b1b57976e2f53e7a
Justin Poehnelt <justin@descarteslabs.com>
2019-07-12 13:48:27 -0600

    inline formatargspec

--- boltons/funcutils.py
+++ boltons/funcutils.py
@@ -519,13 +519,13 @@ class FunctionBuilder(object):
                 annotations = self.annotations
             else:
                 annotations = {}
-            return inspect.formatargspec(self.args,
-                                         self.varargs,
-                                         self.varkw,
-                                         [],
-                                         self.kwonlyargs,
-                                         {},
-                                         annotations)
+            return formatargspec(self.args,
+                                 self.varargs,
+                                 self.varkw,
+                                 [],
+                                 self.kwonlyargs,
+                                 {},
+                                 annotations)
 
         _KWONLY_MARKER = re.compile(r"""
         \*     # a star
@@ -542,14 +542,14 @@ class FunctionBuilder(object):
                                     for arg in self.kwonlyargs)
                 formatters['formatvalue'] = lambda value: '=' + value
 
-            sig = inspect.formatargspec(self.args,
-                                        self.varargs,
-                                        self.varkw,
-                                        [],
-                                        kwonly_pairs,
-                                        kwonly_pairs,
-                                        {},
-                                        **formatters)
+            sig = formatargspec(self.args,
+                                self.varargs,
+                                self.varkw,
+                                [],
+                                kwonly_pairs,
+                                kwonly_pairs,
+                                {},
+                                **formatters)
             sig = self._KWONLY_MARKER.sub('', sig)
             return sig[1:-1]
 
@@ -786,4 +786,55 @@ except ImportError:
                 setattr(cls, opname, opfunc)
         return cls
 
+
+def formatargspec(args, varargs=None, varkw=None, defaults=None,
+                  kwonlyargs=(), kwonlydefaults={}, annotations={},
+                  formatarg=str,
+                  formatvarargs=lambda name: '*' + name,
+                  formatvarkw=lambda name: '**' + name,
+                  formatvalue=lambda value: '=' + repr(value),
+                  formatreturns=lambda text: ' -> ' + text,
+                  formatannotation=inspect.formatannotation):
+    """Format an argument spec from the values returned by getfullargspec.
+
+    The first seven arguments are (args, varargs, varkw, defaults,
+    kwonlyargs, kwonlydefaults, annotations).  The other five arguments
+    are the corresponding optional formatting functions that are called to
+    turn names and values into strings.  The last argument is an optional
+    function to format the sequence of arguments.
+
+    Deprecated since Python 3.5: use the `signature` function and `Signature`
+    objects.
+    """
+
+    def formatargandannotation(arg):
+        result = formatarg(arg)
+        if arg in annotations:
+            result += ': ' + formatannotation(annotations[arg])
+        return result
+    specs = []
+    if defaults:
+        firstdefault = len(args) - len(defaults)
+    for i, arg in enumerate(args):
+        spec = formatargandannotation(arg)
+        if defaults and i >= firstdefault:
+            spec = spec + formatvalue(defaults[i - firstdefault])
+        specs.append(spec)
+    if varargs is not None:
+        specs.append(formatvarargs(formatargandannotation(varargs)))
+    else:
+        if kwonlyargs:
+            specs.append('*')
+    if kwonlyargs:
+        for kwonlyarg in kwonlyargs:
+            spec = formatargandannotation(kwonlyarg)
+            if kwonlydefaults and kwonlyarg in kwonlydefaults:
+                spec += formatvalue(kwonlydefaults[kwonlyarg])
+            specs.append(spec)
+    if varkw is not None:
+        specs.append(formatvarkw(formatargandannotation(varkw)))
+    result = '(' + ', '.join(specs) + ')'
+    if 'return' in annotations:
+        result += formatreturns(formatannotation(annotations['return']))
+    return result
 # end funcutils.py
