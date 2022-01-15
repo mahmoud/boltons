395f690f4a24331c4554e2169ac18a15955a4eab
Allan Lei <allanlei@helveticode.com>
2022-01-16 07:08:40 +0800

    Chaining `boltons.urlutils.URL.navigate()` results in `TypeError` (#298)
    * Add test cases for issue #158
    
    * Fix TypeError w/issue #158 and PR #159
    
    This makes only one more test pass (now only 3 failing) but at least
    prevents the `TypeError` when attempting to add a `list` and a `tuple`.
    
    * Fix: Expected results should be `https://host/b`
    
    * Provide parameters via `path`
    
    * Move test case after `test_navigate`
    
    Co-authored-by: Wil Cooley <wcooley@nakedape.cc>

--- tests/test_urlutils.py
+++ tests/test_urlutils.py
@@ -313,6 +313,25 @@ def test_navigate():
     assert navd.to_text() == _dest_text
 
 
+@pytest.mark.parametrize(
+    ('expected', 'base', 'paths'), [
+    ('https://host/b', 'https://host', ('a', '/b', )),
+    ('https://host/b', 'https://host', ('a', 'b', )),
+    ('https://host/a/b', 'https://host', ('a/', 'b', )),
+    ('https://host/b', 'https://host', ('/a', 'b', )),
+    ('https://host/a/b', 'https://host/a/', (None, 'b', )),
+    ('https://host/b', 'https://host/a', (None, 'b', )),
+])
+def test_chained_navigate(expected, base, paths):
+    """Chained :meth:`navigate` calls produces correct results."""
+    url = URL(base)
+
+    for path in paths:
+        url = url.navigate(path)
+
+    assert expected == url.to_text()
+
+
 # TODO: RFC3986 6.2.3 (not just for query add, either)
 # def test_add_query():
 #     url = URL('http://www.example.com')
