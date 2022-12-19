f5db185c7c47a5c2ac21585107d4faa5eb170e56
Kurt Rose <kurt@kurtrose.com>
2022-12-18 16:53:07 -0800

    bytes and hex both pad to 1 byte so combine test

--- tests/test_mathutils.py
+++ tests/test_mathutils.py
@@ -95,8 +95,9 @@ def test_bits():
     chk(Bits('11') >> 1, Bits('1'))
     chk(Bits('1') << 1, Bits('10'))
     assert Bits('0') != Bits('00')
-    chk(Bits.from_bytes(Bits('10101010').as_bytes()), Bits('10101010'))
-    chk(Bits.from_hex(Bits('1010').as_hex()), Bits('1010'))
+    chk(
+        Bits.from_bytes(Bits.from_hex(Bits('10101010').as_bytes()).as_hex()),
+        Bits('10101010'))
     chk(Bits.from_int(
             Bits.from_bin(
                 Bits.from_list(
