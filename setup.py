d82ecc45035ac4d2e71501e79e8e99eba09ffbb9
Mahmoud Hashemi <mahmoud@hatnote.com>
2023-11-01 00:04:10 -0700

    bump version for 23.1.1 release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '23.1.1dev'
+__version__ = '23.1.1'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/boltons'
 __license__ = 'BSD'
