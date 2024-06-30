99e55b503ec733c4833056cdd86368bf709a226b
Mahmoud Hashemi <mahmoud@hatnote.com>
2024-06-29 23:29:58 -0700

    test iter_find_files max depth

--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -1,4 +1,10 @@
-from boltons.fileutils import FilePerms
+import os.path
+
+from boltons import fileutils
+from boltons.fileutils import FilePerms, iter_find_files
+
+
+BOLTONS_PATH = os.path.dirname(os.path.abspath(fileutils.__file__))
 
 
 def test_fileperms():
@@ -17,3 +23,14 @@ def test_fileperms():
     assert oct(int(up)) == '0o770'
 
     assert int(FilePerms()) == 0
+
+
+def test_iter_find_files():
+    def _to_baseless_list(paths):
+        return [p.lstrip(BOLTONS_PATH) for p in paths]
+
+    assert 'fileutils.py' in _to_baseless_list(iter_find_files(BOLTONS_PATH, patterns=['*.py']))
+
+    boltons_parent = os.path.dirname(BOLTONS_PATH)
+    assert 'fileutils.py' in _to_baseless_list(iter_find_files(boltons_parent, patterns=['*.py']))
+    assert 'fileutils.py' not in _to_baseless_list(iter_find_files(boltons_parent, patterns=['*.py'], max_depth=0))
