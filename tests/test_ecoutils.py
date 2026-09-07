9decba3e5736f61180eec274420ced0e572e30e6
Kevin Deldycke <kdeldycke@users.noreply.github.com>
2026-09-07 10:42:03 +0200

    ecoutils: skip identifying lookups when scrubbing (#461)

--- tests/test_ecoutils.py
+++ tests/test_ecoutils.py
@@ -1,4 +1,7 @@
+import os
+import socket
 import sys
+
 from boltons import ecoutils
 
 
@@ -13,3 +16,39 @@ def test_scrub():
     prof = ecoutils.get_profile(scrub=True)
 
     assert prof['username'] == '-'
+    assert prof['hostname'] == '-'
+    assert prof['hostfqdn'] == '-'
+    assert prof['cwd'] == '-'
+    assert prof['uname']['node'] == '-'
+    assert prof['python']['bin'] == '-'
+    assert prof['python']['argv'] == '-'
+
+
+def test_scrub_does_not_resolve_the_host(monkeypatch):
+    def unreachable(*args):
+        raise AssertionError('scrubbed profile resolved the host name')
+
+    monkeypatch.setattr(socket, 'gethostname', unreachable)
+    monkeypatch.setattr(socket, 'getfqdn', unreachable)
+
+    prof = ecoutils.get_profile(scrub=True)
+
+    assert prof['hostname'] == '-'
+    assert prof['hostfqdn'] == '-'
+
+
+def test_scrub_does_not_read_the_cwd(monkeypatch):
+    calls = []
+    real_getcwd = os.getcwd
+
+    def recording_getcwd():
+        calls.append('getcwd')
+        return real_getcwd()
+
+    monkeypatch.setattr(os, 'getcwd', recording_getcwd)
+
+    prof = ecoutils.get_profile(scrub=True)
+
+    assert calls == []
+
+    assert prof['cwd'] == '-'
